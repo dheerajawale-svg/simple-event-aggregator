a7db506faeaa9d49159db6e1d97d37861235a4f0
Dheeraj-Awale_natus <Dheeraj.Awale@natus.com>
2023-10-08 18:04:14 +0530

    adding project files

---
+++ EventAggregator/EventAggregator/IEventAggregator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAggregator
+{
+    public interface IEventAggregator
+    {
+        void PublishEvent<TEventType>(TEventType eventToPublish);
+
+        void SubsribeEvent(Object subscriber);
+    }
+}
---
+++ EventAggregator/EventAggregator/ISubscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAggregator
+{
+    public interface ISubscriber<TEventType>
+    {
+        void OnEventHandler(TEventType e);
+    }
+}
